5b167fb3c4b8734bdb2a41058087629b83ca93c2
jyona2000 <shinsogreen@gmail.com>
2017-12-05 12:10:05 +0000

    new test.py

---
+++ test.py
@@ -0,0 +1,16 @@
+def application(environ, start_response):
+
+
+
+
+    status = '200 OK'
+
+    header =  [('Content-Type', 'text/html')]
+
+    html = 'hello python world'
+
+    start_response(status,header)
+
+
+
+    return [html.encode('utf-8')]
