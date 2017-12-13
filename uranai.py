569659efe9b65beba1dd218c4f2efb5456166873
jyona2000 <shinsogreen@gmail.com>
2017-12-13 10:44:27 +0000

    urani完成

--- uranai.py
+++ uranai.py
@@ -1,13 +1,24 @@
-from flask import Flask ,request
+from flask import Flask ,request ,render_template
 
 application = Flask(__name__)
+
+@application.route('/show_uranai')
+def show_uranai():
+    text = request.form['name']
+    html = render_template('uranai.html', name=name)
+    print(html)
+    return html
 
 @application.route('/uranai', methods=['POST'])
 def uranai():
     text = request.form['name']
 
-    if text == "taro" :
-        return "dakiti"
+    if text == 'taro' :
+        html = render_template('daikiti.html', name=text)
+        print(html)
+        return html
 
     else:
-        return "kiti"
+        html = render_template('kiti.html', name=text)
+        print(html)
+        return html
